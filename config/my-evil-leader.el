8c800fd302dcaa73fc1353e69396852aec8c26d1
shoneill <oneill.shane.h@gmail.com>
2015-08-17 20:59:06 -0400

    new leader keybind

--- config/my-evil-leader.el
+++ config/my-evil-leader.el
@@ -10,6 +10,7 @@
 (evil-leader/set-key "c" 'kill-buffer-and-window)
 (evil-leader/set-key "h" 'dired-jump)
 (evil-leader/set-key "v" 'split-window-right)
+(evil-leader/set-key "s" 'split-window-below)
 (evil-leader/set-key "e" 'pp-eval-last-sexp)
 (evil-leader/set-key "," 'other-window)
 (evil-leader/set-key "b" 'ibuffer)
