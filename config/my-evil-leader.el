26ae084edde53b18b7e5890f5ebf112695987d93
shoneill <oneill.shane.h@gmail.com>
2015-11-05 14:21:17 -0500

    updates

--- config/my-evil-leader.el
+++ config/my-evil-leader.el
@@ -1,6 +1,5 @@
 ;;
 ;; my-evil-leader.el
-;; shane o'neill
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -9,12 +8,13 @@
 (evil-leader/set-key "w" 'save-buffer)
 (evil-leader/set-key "c" 'kill-buffer-and-window)
 (evil-leader/set-key "z" 'delete-window)
-(evil-leader/set-key "h" 'dired-jump)
+(evil-leader/set-key "h" 'ranger)
 (evil-leader/set-key "v" 'split-window-right)
 (evil-leader/set-key "s" 'split-window-below)
 (evil-leader/set-key "e" 'pp-eval-last-sexp)
 (evil-leader/set-key "," 'other-window)
 (evil-leader/set-key "b" 'ibuffer)
 (evil-leader/set-key "i" 'indent-entire-buffer)
+(evil-leader/set-key "t" 'shell)
 
 (provide 'my-evil-leader)
--- init.el
+++ init.el
@@ -17,7 +17,7 @@
 
 ; packages I use
 (setq package-list '(use-package flyspell auctex evil-leader evil zenburn-theme
-                      solarized-theme neotree))
+                      solarized-theme neotree ranger))
 
 ; fetch the list of packages available 
 (unless package-archive-contents
@@ -71,7 +71,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default))))
+    ("3dafeadb813a33031848dfebfa0928e37e7a3c18efefa10f3e9f48d1993598d3" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
