6fd77676d138a3fdfff987a6d411ab4cb825ed3b
zeev0 <zeevozeevo@gmail.com>
2016-04-04 16:28:59 -0400

    updates

--- config/my-skewer.el
+++ config/my-skewer.el
@@ -26,4 +26,23 @@
  '(js2-error ((t nil)))
  '(js2-warning ((t nil))))
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("20e359ef1818a838aff271a72f0f689f5551a27704bf1c9469a5c2657b417e6c" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+ '(js2-strict-missing-semi-warning t)
+ '(web-mode-enable-auto-closing t)
+ '(web-mode-enable-auto-quoting nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(js2-error ((t nil)))
+ '(js2-warning ((t nil))))
+
 (provide 'my-skewer)
--- init.el
+++ init.el
@@ -58,7 +58,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "20e359ef1818a838aff271a72f0f689f5551a27704bf1c9469a5c2657b417e6c" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(js2-strict-missing-semi-warning t)
  '(web-mode-enable-auto-closing t)
  '(web-mode-enable-auto-quoting nil))
