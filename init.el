d1f34bcbaad4a9269abe9c44c18c90020ff7c4c9
zeev0 <zeevozeevo@gmail.com>
2016-11-15 20:24:33 -0500

    Updates

--- init.el
+++ init.el
@@ -17,7 +17,8 @@
 ; packages I use
 (setq package-list '(use-package auto-complete helm web-mode 
                       js2-mode magit powerline auctex latex-preview-pane evil-magit evil-org flyspell
-                      evil-leader evil spacemacs-theme zenburn-theme solarized-theme neotree
+                      evil-leader evil spacemacs-theme fireplace
+                      grandshell-theme zenburn-theme solarized-theme neotree
                       dired-hacks-utils dired-ranger))
 
 ; fetch the list of packages available 
@@ -28,8 +29,6 @@
 (dolist (package package-list)
     (unless (package-installed-p package)
           (package-install package)))
-
-(require 'euphoria-theme)
 
 (require 'use-package)
 
@@ -60,9 +59,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#0a0814" "#f2241f" "#67b11d" "#b1951d" "#4f97d7" "#a31db1" "#28def0" "#b2b2b2"])
  '(custom-safe-themes
    (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
+    ("b9293d120377ede424a1af1e564ba69aafa85e0e9fd19cf89b4e15f8ee42a8bb" "2047464bf6781156ebdac9e38a17b97bd2594b39cfeaab561afffcbbe19314e2" "2d5c40e709543f156d3dee750cd9ac580a20a371f1b1e1e3ecbef2b895cf0cd2" "80a23d559a5c5343a0882664733fd2c9e039b4dbf398c70c424c8d6858b39fc5" "7997e0765add4bfcdecb5ac3ee7f64bbb03018fb1ac5597c64ccca8c88b1262f" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(markdown-command "/usr/bin/pandoc"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
